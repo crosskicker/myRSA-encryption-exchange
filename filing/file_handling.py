ad3831bbbcb9885d73abfbab1ee6ec748d7f62ac
crosskicker <121888262+crosskicker@users.noreply.github.com>
2024-10-02 12:08:13 +0200

    Update file_handling.py

--- filing/file_handling.py
+++ filing/file_handling.py
@@ -1,31 +1,33 @@
 import json
 
-def create_json_pbKey(pb_key,name):
+def create_json_pbKey(pb_key, name):
     public_key_data = {
-    'n': pb_key[0],
-    'e': pb_key[1]
+        'n': pb_key[0],
+        'e': pb_key[1]
     }
-    with open('public_key_'+ name +'.json', 'w') as f:
+    # Write the public key data to a JSON file
+    with open('public_key_' + name + '.json', 'w') as f:
         json.dump(public_key_data, f)
-    with open('public_key_'+ name +'.json', 'rb') as f:
+    # Read the data back from the file in binary mode
+    with open('public_key_' + name + '.json', 'rb') as f:
         file_data = f.read()
-    print("size file : ",len(file_data))
+    #print("size file : ", len(file_data))
     return file_data
 
 def receive_json(socket, name):
-    file_size = 636 #change if you change size key
-    # Recevoir les données du fichier
+    file_size = 1252  # Change if you change the key size RSA-1024 -> 636 ; RSA-2048 -> 1252
+    # Receive the file data
     file_data = b""
     while len(file_data) < file_size:
-        packet = socket.recv(4096)  # Recevoir par blocs de 4096 bytes
+        packet = socket.recv(4096)  # Receive data in blocks of 4096 bytes
         if not packet:
             break
         file_data += packet    
-    # Écrire les données reçues dans un fichier
-    with open('received_'+ name +'.json', 'wb') as f:
+    # Write the received data to a file
+    with open('received_' + name + '.json', 'wb') as f:
         f.write(file_data)
-    # Lire le fichier JSON et récupérer les valeurs de la clé publique
-    with open('received_'+ name +'.json', 'r') as f:
+    # Read the JSON file and extract the public key values
+    with open('received_' + name + '.json', 'r') as f:
         public_key_data = json.load(f)
 
     n = public_key_data['n']
