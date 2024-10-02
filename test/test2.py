6346247f8456cea08b3021cf9ccc6aa1c7636f68
crosskicker <alban.personnaz@gmail.com>
2024-10-02 11:56:46 +0200

    v1

--- test/test2.py
+++ test/test2.py
@@ -6,6 +6,9 @@ from cryptage.crypting import *
 
 #performance test
 import time 
+public_key, private_key = generate_rsa_keys(bits=1024)  # RSA-1024 (because n = 1024 bits)
+
+
 message = "bonjour tout le monde"
 msg = int.from_bytes(message.encode('utf-8'), 'big')
 n, e = public_key
@@ -15,18 +18,16 @@ pow(msg, e, n)
 end = time.time()  
 
 
-st2 = time.time()
+""" st2 = time.time()
 #fastExp(msg,intToBinExp(e)) % n
 public_key, private_key = generate_rsa_keys()
 en2 = time.time()
 
 print(end - start)
-print(en2 - st2)
-
-public_key, private_key = generate_rsa_keys()
+print(en2 - st2) """
 
 ########### test #########""
-msg_test = "bonjour le monde"
+""" msg_test = "bonjour le monde"
 msg_code = encrypt_rsa(msg_test, public_key)
 print("voici le msg crypte : " , msg_code)
 
@@ -34,4 +35,4 @@ msg_dec = decrypt_rsa(msg_code, private_key)
 print("voici le msg decrypte : " , msg_dec) 
 
 #cle public 
-print("voici ma cle public : " , public_key)
+print("voici ma cle public : " , public_key) """
